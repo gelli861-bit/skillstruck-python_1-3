579483cb51dca2f46077d11e1da4c91cd24ddcf7
gelli861-bit <gelli861@evit.edu>
2026-08-14 10:18:55 -0700

    exported from Skill Struck

---
+++ Add_the_Factorials.py
@@ -0,0 +1,16 @@
+number = int(input("What number do you want to know?"))
+
+mult_total = 1
+sum_total = 0
+
+for x in range(1, number+1):
+
+
+# A factoril is all the nubers in a range multiplied together.
+#5 = 5 * 4 * 3 * 2 * 1 (And so on. not including 0 or negatives.)
+#This project wants to ADD the factorials together.
+
+# "!" is the symbol for factorial...
+
+#This is so puzzling... 
+#
