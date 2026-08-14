a2cc8c451ac1b00d2def44055979962b878a3e3c
gelli861-bit <gelli861@evit.edu>
2026-08-14 10:18:48 -0700

    exported from Skill Struck

---
+++ Sentence_Generator.py
@@ -0,0 +1,3 @@
+noun = ["pet ", "dog ", "cat ", "mouse ", "fish ", "shoe ", "mind ", "bomb ", "doll ", "human "]
+for x in noun:
+    print("While i was taking my " + x + "for a walk, another giant " + x + "came out of nowhere and chased me all the way home!")
