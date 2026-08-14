ec87d23dcc89f3028c1891096ad356c6338c1dee
gelli861-bit <gelli861@evit.edu>
2026-08-14 10:18:47 -0700

    exported from Skill Struck

---
+++ Python_For_Loops.py
@@ -0,0 +1,9 @@
+numb1 = int(input("Enter a number: "))
+numb2 = int(input("Enter another number: "))
+numb3 = 0
+for x in range(numb1, numb2 + 1):
+    numb3 = numb3 + x
+print(numb3)
+
+#To sum up, Loops are able to read lists and ranges, and generate responses by repeating the output by the legth of the list or range.
+#I May update this later with a better explanation.
