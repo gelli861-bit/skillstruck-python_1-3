196d9e434fd6c463da88aa3c6e55ed4d3ed406ed
gelli861-bit <gelli861@evit.edu>
2026-08-14 10:18:28 -0700

    exported from Skill Struck

---
+++ Input.py
@@ -0,0 +1,5 @@
+Prompt = input("Please Enter a Question.")
+
+print(Prompt)
+print("This is additional text that asks you " + Prompt)
+print("This sentence ends with an exclamation mark. " + Prompt + "!")
