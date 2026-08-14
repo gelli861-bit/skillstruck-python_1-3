456a41a209ac6379c666c886a63326d3398adb75
gelli861-bit <gelli861@evit.edu>
2026-08-14 10:18:44 -0700

    exported from Skill Struck

---
+++ Python_If_Statements.py
@@ -0,0 +1,10 @@
+#Simple answer: REMEMBER SCRATCH!!!
+
+#It really is just the "if {} then " and the "if {} then, else" statements, just typed out in python.
+
+scratch = 0
+python = 1
+if scratch == python:
+    print("No it isn't.")
+else:
+    print("It looks different, but it isn't, it works the same, just typed out.")
