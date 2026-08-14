140afa52961ad1a37d8e0a7e556911e73ff3d9df
gelli861-bit <gelli861@evit.edu>
2026-08-14 10:18:41 -0700

    exported from Skill Struck

---
+++ Story_Maker.py
@@ -0,0 +1,57 @@
+string1 = "One day "
+string2 = "This morning "
+string3 = "Yesterday "
+string4 = "my shoelace "
+string5 = "my pet gopher "
+string6 = "my teacher "
+string7 = "my mom "
+string8 = "my eyeball "
+string9 = "wasn't feeling well "
+string10 = "slipped on a banana peel "
+string11 = "had a little too much sugar "
+string12 = "won a trophy because "
+string13 = "drove a racecar "
+string14 = "in "
+string15 = "under "
+string16 = "on top of "
+string17 = "next to "
+string18 = "a hedgehog "
+string19 = "an orange beetle "
+string20 = "a snail "
+string21 = "a kangaroo "
+string22 = "a radish "
+string23 = "got braces "
+string24 = "a golden tooth "
+string25 = "went to the circus "
+string26 = "attacked "
+string27 = "looked "
+string28 = "rescued "
+string29 = "created "
+string30 = "trapped "
+string31 = "invented "
+string32 = "promised "
+string33 = "their "
+string34 = "his "
+string35 = "her "
+string36 = "a "
+string37 = "and "
+string38 = "then "
+string39 = "meanwhile "
+string40 = "unfortunately "
+string41 = "because "
+string42 = "got "
+string43 = "who "
+string44 = "he "
+string45 = "she "
+
+story = string2 + string6 + string42 + string24 + string41 + string44 + string11 + string37 + string10 + string3
+story2 = string1 + string7 + string9 + string37 + string26 + string5 + string41 + string44 + string30 + string4
+
+print(story)
+
+print(story2)
+
+print(story.upper())
+print(story2.lower())
+
+print(story.replace(string11, string25))
