443f081a63359fd0a0f6d5110bf9c3a608c36169
gelli861-bit <gelli861@evit.edu>
2026-08-14 10:19:23 -0700

    exported from Skill Struck

---
+++ Stacks.py
@@ -0,0 +1,38 @@
+response = input("What food should we bring?")
+                 
+answer = ["apples", "steak", "potatoes", "carrots"]
+
+if "s" in response:
+    answer.append(response)
+    print(answer)
+else:
+    print("The input doesn't have the letter s")
+
+
+
+# first = "r"
+# second = "t"
+# third = "s"
+# fourth = "y"
+# fifth = "o"
+
+# answer = []
+
+# answer.append(third)
+# answer.append(second)
+# answer.append(fifth)
+# answer.append(first)
+# answer.append(fourth)
+
+# print(answer)
+
+
+# desserts = []
+
+# desserts.append("cake")
+# desserts.append("ice cream")
+# desserts.append("cookies")
+# desserts.append("brownies")
+# desserts.pop()
+
+# print(desserts)
