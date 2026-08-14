7e6bcc7cfc5d7a5792e235ab414ad556c4c6472e
gelli861-bit <gelli861@evit.edu>
2026-08-14 10:18:44 -0700

    exported from Skill Struck

---
+++ Team_Roster.py
@@ -0,0 +1,15 @@
+print("Joe " + str(1))
+print("Madeline " + str(2))
+print("Jeremy " + str(3))
+print("Ahmend " + str(4))
+print("Li " + str(5))
+print("Sarah " + str(6))
+print("Xander " + str(7))
+print("Jane " + str(8))
+print("Savannah " + str(9))
+print("Juan " + str(10))
+
+string1 = "Welcome to the team! Your team number is {}."
+team_number = 11
+
+print(string1.format(team_number))
