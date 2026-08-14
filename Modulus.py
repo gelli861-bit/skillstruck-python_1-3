9cc824efba4c8149fd0230b6dc85496227012501
gelli861-bit <gelli861@evit.edu>
2026-08-14 10:18:34 -0700

    exported from Skill Struck

---
+++ Modulus.py
@@ -0,0 +1,4 @@
+#The Modulus symbol "%" is used to define the remainder of a division problem, and when used in a print statement, prints the remainder of said problem
+
+print(7 % 3)
+#The output of this would be 1
