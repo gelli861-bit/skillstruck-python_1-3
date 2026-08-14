ec3bd3108af944f315d29d2b7a64a72f26805892
gelli861-bit <gelli861@evit.edu>
2026-08-14 10:18:32 -0700

    exported from Skill Struck

---
+++ Concatenation.py
@@ -0,0 +1,11 @@
+variable1 = "Sample"
+variable2 = "Text"
+
+#These two variables can be put together using Concatenation.
+#Essentially, put a place inbetween them when printing.
+
+#However, if you want the sentence to have a space, you have two options. Either:
+#A. Add a space to the end of variable1, or at the beggining of variable2.
+#B. in the print statement, add another + and inbetween the two variables, enter a string with just a space, like this: " "
+
+print(variable1 + " " + variable2)
