d96766ccf92dc55a01520b3fa3eb13bf247aae12
gelli861-bit <gelli861@evit.edu>
2026-08-14 10:18:29 -0700

    exported from Skill Struck

---
+++ Syntax_Comments.py
@@ -0,0 +1,4 @@
+# This is the editing of many Words reused at once, which selects every instance of said Words, and lets you change them. these Words will then change into whatever you desire the Words to be.
+
+print("Describe this code")
+# This will type the words in the quotation marks onto the script runner, every letter to the exact as you typed it.
