712205dd7000fb04721efc2ee01090ee55032ea7
gelli861-bit <gelli861@evit.edu>
2026-08-14 10:18:45 -0700

    exported from Skill Struck

---
+++ Python_Conditionals.py
@@ -0,0 +1,25 @@
+# Remember These symbols:
+
+>
+#Greater than
+
+>=
+#Greater than or equal to
+
+<
+#Less than
+
+<=
+#Less than or equal to
+
+==
+#Equal to (Not just one "=")
+
+!=
+#Not equal to
+
+and
+#Condition1 AND Condition2
+
+or
+#Condition1 OR Condition2
